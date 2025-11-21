db1b1f2238d215586793a99ce9354ce3eeefa997
Nivekyareg17 <guevaraclavijok@gmail.com>
2025-11-21 16:51:34 -0500

    versionBeta

---
+++ src/main/java/com/example/segurosMeca_col/SegurosMecaColApplication.java
@@ -0,0 +1,13 @@
+package com.example.segurosMeca_col;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurosMecaColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurosMecaColApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/segurosMeca_col/SegurosMecaColApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.segurosMeca_col;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurosMecaColApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
