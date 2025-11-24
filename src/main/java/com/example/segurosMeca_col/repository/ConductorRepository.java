82aaa78a756acb6d5c50457af505f99b143dea8d
EstebanEsc2025 <estebanescarpetta@gmail.com>
2025-11-24 16:44:57 -0500

    Actualizacion 3.5.1

---
+++ src/main/java/com/example/segurosMeca_col/repository/ConductorRepository.java
@@ -0,0 +1,6 @@
+package com.example.segurosMeca_col.repository;
+
+public interface ConductorRepository {
+
+    Boolean existBySeguroName(String id_Seguro);
+}
---
+++ src/main/java/com/example/segurosMeca_col/repository/SeguroRepository.java
@@ -0,0 +1,5 @@
+package com.example.segurosMeca_col.repository;
+
+public interface SeguroRepository {
+    Boolean existsByConductorName(String nombre);
+}
